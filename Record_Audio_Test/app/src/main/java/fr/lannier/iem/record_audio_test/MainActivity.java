d8d49cc0b3d92bdfc49048ea8e55212d4b4cbaa2
Unknown <johanlannier@gmail.com>
2018-01-29 22:36:28 +0100

    add chronometer total duration + alertdialog recap

--- Record_Audio_Test/app/src/main/java/fr/lannier/iem/record_audio_test/MainActivity.java
+++ Record_Audio_Test/app/src/main/java/fr/lannier/iem/record_audio_test/MainActivity.java
@@ -1,13 +1,15 @@
 package fr.lannier.iem.record_audio_test;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.SystemClock;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -24,24 +26,24 @@ public class MainActivity extends AppCompatActivity {
     long timeWhenStopped = 0;
     Button btnStop;
     Button btnStart;
+    Chronometer totalChrono;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         chrono=(Chronometer) findViewById(R.id.chronometer);
+        totalChrono=(Chronometer) findViewById(R.id.chronometerTotal);
         sm=new SoundMeter(MainActivity.this);
         th=new Thread(sm);
 
-
-
-        final TextView tvData=(TextView) findViewById(R.id.data);
 
         btnStart = (Button)findViewById(R.id.button);
         btnStart.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvData.setText("Écoute en cours...");
+                totalChrono.setBase(SystemClock.elapsedRealtime());
+                totalChrono.start();
                 sm=new SoundMeter(MainActivity.this);
                 timeWhenStopped=0;
                 th=new Thread(sm);
@@ -60,16 +62,28 @@ public class MainActivity extends AppCompatActivity {
         btnStop.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
+                totalChrono.stop();
                 chrono.stop();
                 writeToFile(sm.getData());
                 sm.stopChrono();
-                tvData.setText("Durée: "+chrono.getText());
                 btnStop.setTextSize(10);
                 btnStart.setTextSize(25);
                 btnStop.setEnabled(false);
                 btnStart.setEnabled(true);
                 btnStart.setBackgroundResource(R.drawable.round_button_start);
                 btnStop.setBackgroundResource(R.drawable.round_button_stop_disabled);
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Temps de parole: "+chrono.getText()+"\n\nDurée totale: "+totalChrono.getText());
+                builder.setTitle("Récapitulatif");
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+                Toast.makeText(MainActivity.this, "Temps de parole: "+chrono.getText()+", total: "+totalChrono.getText(), Toast.LENGTH_SHORT).show();
             }
         });
     }
